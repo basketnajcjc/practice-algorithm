9eb2c6123a9e355dc9465ec0591a1ce97f43afaa
Developer PJC <basektnajcjc@gmail.com>
2019-08-13 16:26:28 +0900

    * 2019. 08. 12 알고리즘 SpecialSortingTwo

--- codingDojang/src/codingDojang/SpecialSortingTwo.java
+++ codingDojang/src/codingDojang/SpecialSortingTwo.java
@@ -1,5 +1,8 @@
 package codingDojang;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Park Jae Cheul
  *
@@ -12,6 +15,36 @@ public class SpecialSortingTwo {
 	 * 
 	 * 다음과 같은 형태로 바꾸시오
 	 * [a1,b1,a2,b2.....an,bn]
+	 * 
 	 */
+	
+	public static void main(String[] args) {
+		String[] array = {"a1", "b3", "a2", "b1", "a3", "b4", "a4", "b2"};
+		String temp = "";
+		
+		for(int i=0; i<array.length; i++) {
+			for(int j=i; j<array.length; j++) {
+				
+				// 오름차순 정열
+				if(array[i].compareTo(array[j]) > 0) {
+					temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+				}
+				
+				// 숫자에 대해 한 번 더 정열
+				if(array[i].substring(1,2).compareTo(array[j].substring(1,2)) > 0 ) {
+					temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+				}
+				
+			}
+		}
+		
+		for(int i=0; i<array.length; i++) {
+			System.out.println(array[i]);
+		}
+	}
 	
 }
